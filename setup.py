20184c1d421b9c687f047404dc8720e78b56cd1e
ankitk75 <ankitk7519@gmail.com>
2025-08-26 00:40:31 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankit Kumar",
+    author_email="ankitk7519@gmail.com",
+    packages=find_packages()
+)
